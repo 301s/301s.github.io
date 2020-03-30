dfc4ec46f3aad83d3654f2877300177d6359df92
Azeez Adewale <azeezadewale98@gmail.com>
2020-03-30 12:34:23 +0100

    add cronux, and devjammer shortlinks

--- assets/js/urls.1.js
+++ assets/js/urls.1.js
@@ -8,12 +8,14 @@ if (typeof window.URLMaps == 'undefined') {
 const urlMap1 = {
     1 : "https://thecarisma.github.io/",
     2 : "https://github.com/Thecarisma",
+    3 : "https://thecarisma.github.io/installx.ps1",
     lt : "https://libopenprojects.github.io/libopen-types",
     ln : "https://libopenprojects.github.io/libopen-notify",
     lp : "https://libopenprojects.github.io/libopen-processes",
     ls : "https://libopenprojects.github.io/sampleprograms",
     lgp : "https://libopenprojects.github.io/",
     dc : "https://www.patreon.com/devcareer",
+    dj : "https://thedarkprojects.github.io/devjammer/",
     exit : "https://301s.github.io/"
 }
 
