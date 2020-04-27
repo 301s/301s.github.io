17f40530eb41103e84d6f5a0c33e28e524e0c632
Azeez Adewale <azeezadewale98@gmail.com>
2020-04-27 12:31:14 +0100

    add the konfiger shortlinks

--- assets/js/urls.1.js
+++ assets/js/urls.1.js
@@ -9,12 +9,25 @@ const urlMap1 = {
     1 : "https://thecarisma.github.io/",
     2 : "https://github.com/Thecarisma",
     3 : "https://thecarisma.github.io/installx.ps1",
+    
+    // libexotic
     lt : "https://exoticlibraries.github.io/libexotic-types",
     ln : "https://exoticlibraries.github.io/libexotic-notify",
     lp : "https://exoticlibraries.github.io/libexotic-processes",
     ls : "https://exoticlibraries.github.io/sampleprograms",
     lc : "https://exoticlibraries.github.io/libcester",
     lgp : "https://exoticlibraries.github.io/",
+    
+    // konfiger
+    kn : "https://konfiger.github.io/konfiger-nodejs",
+    kj : "https://konfiger.github.io/konfiger-java",
+    kp : "https://konfiger.github.io/konfiger-python",
+    kcs : "https://konfiger.github.io/konfiger-csharp",
+    kc : "https://konfiger.github.io/konfiger-c",
+    kb : "https://konfiger.github.io/konfiger-beam",
+    kgp : "https://konfiger.github.io",
+    
+    // others
     dc : "https://www.patreon.com/devcareer",
     dj : "https://thedarkprojects.github.io/devjammer/",
     exit : "https://301s.github.io/"
